d608a04f396d90f9cc015308c4d877182474acff
pberube <pberube@handyem.com>
2013-11-11 17:03:53 -0500

    Creation of the project

---
+++ src/cljscripting/core.clj
@@ -0,0 +1,14 @@
+(ns cljscripting.core
+  (:require [clojure.java.io :as io])
+  (:gen-class :main true))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+(defn -main
+  "The application's main function"
+  [& args]
+  (foo "Philippe"))
